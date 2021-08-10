f957978a3ab75a57f0f07c497d17ca881e36d0b6
Carlos-SSH <carlos.anezeh@me.com>
2021-08-10 12:01:25 -0500

    Add a function that displays the list of books

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-library = [];
+let library = [];
 
 function addBook(){
     const book = {};
     book.title = document.getElementById('bookTitle').value;
     book.author = document.getElementById('bookAuthor').value;
     library.push(book);
+    displayBooks();
 }
 
 function removeBook(title){
-    library = library.filter(function(book){
-        return book.title !== title;
+    console.log(title);
+    library = library.filter((book) => book.title !== title);
+    displayBooks();
+}
+
+function displayBooks(){
+    const libraryDiv = document.getElementById('library');
+    libraryDiv.innerHTML = '';
+    library.map((book) => {
+        const bookDiv = document.createElement('div');
+        const p = document.createElement('p');
+        p.innerHTML = book.author;
+        const p2 = document.createElement('p');
+        p2.innerHTML = book.title;
+        const hr = document.createElement('hr');
+        const btn = document.createElement('button');
+        btn.innerHTML = 'Remove';
+        btn.addEventListener('click', () => {
+            removeBook(book.title);
+        });
+        bookDiv.appendChild(p);
+        bookDiv.appendChild(p2);
+        bookDiv.appendChild(hr);
+        bookDiv.appendChild(btn);
+        libraryDiv.appendChild(bookDiv);
+        return book;
+
     });
 }
