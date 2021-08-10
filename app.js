e2f037d3d1af525256c3d255b85b0f670fd99af9
Carlos-SSH <carlos.anezeh@me.com>
2021-08-10 13:03:19 -0500

    Fixed issues in localstorage.
    call the onclick action in the html file.

--- app.js
+++ app.js
@@ -1,49 +1,51 @@
 let library = [];
 
-function addBook(){
+function addBook() {
     const book = {};
-    book.title = document.getElementById('bookTitle').value;
     book.author = document.getElementById('bookAuthor').value;
+    book.title = document.getElementById('bookTitle').value;
     library.push(book);
     displayBooks();
+    SaveLibrary();
 }
 
-function removeBook(title){
+function removeBook(title) {
     console.log(title);
     library = library.filter((book) => book.title !== title);
     displayBooks();
+    SaveLibrary();
 }
 
-function displayBooks(){
+function displayBooks() {
     const libraryDiv = document.getElementById('library');
     libraryDiv.innerHTML = '';
     library.map((book) => {
-        const bookDiv = document.createElement('div');
-        const p = document.createElement('p');
-        p.innerHTML = book.author;
-        const p2 = document.createElement('p');
-        p2.innerHTML = book.title;
-        const hr = document.createElement('hr');
-        const btn = document.createElement('button');
-        btn.innerHTML = 'Remove';
-        btn.addEventListener('click', () => {
-            removeBook(book.title);
-        });
-        bookDiv.appendChild(p);
-        bookDiv.appendChild(p2);
-        bookDiv.appendChild(hr);
-        bookDiv.appendChild(btn);
-        libraryDiv.appendChild(bookDiv);
-        return book;
+      const divBook = document.createElement('div');
+      const p = document.createElement('p');
+      p.innerHTML = book.author;
+      const p2 = document.createElement('p');
+      p2.innerHTML = book.title;
+      const hr = document.createElement('hr');
+      const btn = document.createElement('BUTTON');
+      btn.innerHTML = 'Remove';
+      btn.addEventListener('click', () => {
+        removeBook(book.title);
+      });
+      divBook.appendChild(p);
+      divBook.appendChild(p2);
+      divBook.appendChild(hr);
+      divBook.appendChild(btn);
+      libraryDiv.appendChild(divBook);
+      return book;
+    })
+  }
 
-    });
-}
 
-window.onload = () => {
-    library = JSON.parse(localStorage.getItem('library'));
+window.onload = function() {
+    library = JSON.parse(localStorage.getItem('library') || '[]');
     displayBooks();
 }
 
-function saveLibrary(){
+function SaveLibrary() {
     localStorage.setItem('library', JSON.stringify(library));
 }
